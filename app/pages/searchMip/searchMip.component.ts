ef94ccdcb56c763983ea1aa7992519dd61afb77d
Adrian Ferreres <ardiadrianadri@gmail.com>
2016-10-06 16:11:39 +0200

    Scann screen complete

--- app/pages/searchMip/searchMip.component.ts
+++ app/pages/searchMip/searchMip.component.ts
@@ -1,24 +1,25 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MipEntity } from '../../shared/mipEntity';
 import {SetupItemViewArgs} from "nativescript-angular/directives";
 import * as bluetooth from 'nativescript-bluetooth';
 import { PermissionsService } from '../../shared/permissionsMip.service';
+import { ServiceMip } from '../../shared/serviceMip.service';
+import { RouterExtensions } from "nativescript-angular/router";
 
 
 @Component({
     selector:'mip-search',
     templateUrl: 'pages/searchMip/searchMip.html',
-    styleUrls: ['pages/searchMip/searchMip.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    styleUrls: ['pages/searchMip/searchMip.css']
 })
 export class SearchMip implements OnInit {
     public devices:Array<MipEntity>;
-
-    constructor(private permService:PermissionsService){}
+   
+    constructor(private _permService:PermissionsService, private _mipService:ServiceMip, private _router:RouterExtensions){}
 
     ngOnInit (){
         this.devices = [];
-        this.permService.getPermissions()
+        this._permService.getPermissions()
         .then(()=>{console.log('Permissions granted')})
         .catch((error)=>{
             console.log('Permissions not granted');
@@ -26,8 +27,49 @@ export class SearchMip implements OnInit {
         });
     }
 
+    public search(activityIndicator){
+        let self = this;
+        self.devices=[];
+        activityIndicator.busy=true;
+        bluetooth.startScanning({
+            serviceUUIDs: [],
+            seconds: 10,
+            onDiscovered: (device) => {
+                let deviceFound:MipEntity = new MipEntity();
+                deviceFound.name = device.name;
+                deviceFound.UUID = device.UUID;
+                self.devices.push(deviceFound);
+            }
+        })
+        .then(()=>{
+            console.log("Value of searching: " + activityIndicator.busy);
+            if (activityIndicator.busy) {
+                activityIndicator.busy= false;
+                console.log ("Removing searching title: " + activityIndicator.busy);
+            }
+
+            console.log('Scanning compleate');
+        })
+        .catch(error=>{ 
+             if (activityIndicator.busy) {
+                activityIndicator.busy= false;
+            }
+            throw new Error(error); 
+        });
+    }
+
     public deviceSelected(args){
-        console.log('###' + JSON.stringify(this.devices[args.index]));
         console.log ('Device selected: ' + this.devices[args.index].name);
+        this._mipService.connect(this.devices[args.index].UUID)
+        .then(
+            ()=>{ this._router.navigate(['/control'],{
+                transition:{
+                    name:'flip',
+                    duration:2000,
+                    curve:'linear'
+                }
+            })}
+        )
+        .catch(error=>{ throw new Error (error); });
     }
 }
--- app/shared/serviceMip.service.ts
+++ app/shared/serviceMip.service.ts
@@ -3,57 +3,32 @@ import * as bluetooth from 'nativescript-bluetooth';
 
 import { PermissionsService } from './permissionsMip.service';
 
-const MIP_UUID:string='1C:05:43:5C:E3:54';
-
 @Injectable()
 export class ServiceMip {
 
     private _connection = null;
-    private _hasPermisions = false;
 
     constructor (private _servicePerm:PermissionsService ){}
 
 
-    private _connect(){
-        
-        let self = this;
+    public connect(uuid:string):Promise<any>{
 
-        self._connection = new Promise ((resolve, reject) => {
+        return new Promise ((resolve, reject) => {
            bluetooth.connect({
-               UUID: MIP_UUID,
+               UUID: uuid,
                onConnected: (mip) => {
                    console.log(`Value of BLE obj: ${JSON.stringify(mip)}`)
                    resolve();
                },
                onDisconnected: function () {
-                   self._connection = null;
+                  console.log('Disconected');
                }
            });
         });
     }
 
-    private _doConnection(){
-
-        if (this._hasPermisions){
-            this._connect();
-        } else {
-            this._servicePerm.getPermissions()
-            .then(()=>{
-                this._hasPermisions=true;
-                this._connect();
-            })
-            .catch(error=>{
-                this._hasPermisions=false;
-                throw new Error(error);
-            });
-        }
-    }
-
     move (codeMove:number, codeSpeed:number){
 
-        if (!this._connection){
-            this._doConnection();
-        }
     }
 
 }
