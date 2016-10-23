3f81e6b6adc621a30e85531972b7f90ed4e849ea
Adrian Ferreres <ardiadrianadri@gmail.com>
2016-10-23 16:32:25 +0200

    Scann complete

--- app/shared/permissionsMip.service.ts
+++ app/shared/permissionsMip.service.ts
@@ -1,8 +1,11 @@
 import {Injectable} from '@angular/core';
 import * as bluetooth from 'nativescript-bluetooth';
+import { MipEntity } from './mipEntity';
 
 @Injectable()
 export class PermissionsService {
+
+    public devices:MipEntity[] = [];
 
     public getPermissions():Promise<any>{
         return new Promise((resolve, reject)=>{
@@ -17,6 +20,20 @@ export class PermissionsService {
                 }
             })
             .catch(error=>{ throw new Error(error); });
+        });
+    }
+
+    public scanDevices():Promise<any>{
+        return bluetooth.startScanning({
+            serviceUUIDs: ['fff0'],
+            seconds: 10,
+            onDiscovered: (device)=>{
+                let mipDevice:MipEntity = new MipEntity();
+                mipDevice.name = device.name;
+                mipDevice.UUID = device.UUID;
+
+                this.devices.push(mipDevice);
+            }
         });
     }
 }
